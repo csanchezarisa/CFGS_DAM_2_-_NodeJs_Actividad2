16d8bcf34c99a9fd8645d58cf39cefdddb85c8d2
ulkiobal <ulkiobal@users.noreply.github.com>
2021-05-06 19:48:15 +0200

    Página de error y login

--- index.js
+++ index.js
@@ -20,4 +20,16 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.render('landingpage');
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/juguetes');
+});
+
+app.all('*', (req, res) => {
+    res.render('error');
+});
+
 app.listen(3000);
